5f8e69079da6eb7cbc30e0f9443121e4fcd40f98
Frankie <frankperullo@gmail.com>
2018-12-31 14:44:54 -0500

    fixed slider and added captions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,41 +7,15 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
         @yield('assets')
         <style>
-            @media (min-width: 992px) {
-                .navbar-brand {
-                    margin-left:100px;
-                }
-
-                ul.navbar-nav {
-                    margin-right:100px;
-                }
-            }
-            body {
-                background-color: #fefefe;
-            }
-            nav {
-                position: absolute;
-                top:0;
-                left:0;
-                background-image: linear-gradient(to bottom, rgba(0,255,0,0.2), rgba(0,52,0,0));
-            }
-            nav > a > span  {
-                color: white;
-                font-size:1.5rem;
-                font-weight: bold;
-            }
-            a.nav-link {
-                color: white !important;
-            }
-            a.nav-link:hover {
-                color: #cecece !important;
+            .wrapper {
+                overflow: hidden;
             }
         </style>
     </head>
     <body>
         <!-- NAVBAR -->
 
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a class="navbar-brand" href="/">
                     <span>TJ's Glass</span>
                 </a>
@@ -54,27 +28,22 @@
                       <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#team">Team</a>
+                      <a class="nav-link" href="#about">About Us</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="/services">Services</a>
+                      <a class="nav-link" href="#services">Services</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/partners">Partners</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/blog">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/contact">Contact</a>
+                        <a class="nav-link" href="#contact">Contact</a>
                     </li>
                   </ul>
                 </div>
               </nav>
 
               <!-- BODY -->
-        @yield('content')
-
+        <div class="wrapper">
+            @yield('content')
+        </div>
         <!-- FOOTER -->
             
         <!-- BOOTSTRAP -->
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,8 +10,9 @@
 @endsection
 
 @section('content')
+<div data-spy="scroll" data-target="#main-navbar" data-offset="0">
 
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -20,6 +21,10 @@
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="http://lorempixel.com/1920/1080" alt="First slide">
+        <div class="carousel-caption">
+            <h5>This is slide 1</h5>
+            <p>with some smaller text</p>
+        </div>
       </div>
       <div class="carousel-item">
         <img src="https://picsum.photos/1920/1080" alt="Second slide">
@@ -38,6 +43,16 @@
     </a>
   </div>
 
+  <div id="about" class="row">
+    <div class="col-md-6 bg-primary p-5">
+        <p class="text-white">Blue</p>
+    </div>
+    <div class="col-md-6 bg-danger p-5">
+        <p class="text-white">Red</p>
+    </div>
+  </div>
+
+</div>
 @endsection
 
 @section('scripts')
